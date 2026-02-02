866efa2df5d1819cfb634d774cfa007077a66881
Charan908515 <ncharankumareddy@gmail.com>
2026-02-02 11:43:46 +0530

    changed the storty-tv config

--- config/campaigns.config.js
+++ config/campaigns.config.js
@@ -33,7 +33,7 @@ module.exports = {
             // ----------------------------------------
             affiliate: {
                 baseUrl: 'https://aff.pro-campaign.in/c',
-                offerId: 31,
+                offerId: 38,
                 affiliateId: 49,
                 clickIdParam: 'aff_click_id',
                 // Legacy support if needed server-side
