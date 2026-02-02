e7d09d840b34c7de900de9d937bac97f03e5abe6
Charan908515 <ncharankumareddy@gmail.com>
2026-02-02 11:57:16 +0530

    fh

--- config/campaigns.config.js
+++ config/campaigns.config.js
@@ -77,7 +77,7 @@ module.exports = {
             // ----------------------------------------
             branding: {
                 logoText: 'Story TV',
-                tagline: 'buy Trail get 23 rupees',
+                tagline: 'Install and Ruy Trail get 25 rupees',
                 campaignDisplayName: 'Story TV Offer'
             },
 
